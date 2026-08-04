1ca9e05af1d70ffa668546ebad1302bf5edf3fb1
QR Attendance <ipeyaja43@gmail.com>
2026-08-04 13:51:06 +0700

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -221,7 +221,9 @@ console.log("SEBELUM FETCH");
 
 const res = await fetch(GAS_URL, {
     method: "POST",
-    mode: "no-cors",
+    headers: {
+        "Content-Type": "text/plain;charset=utf-8"
+    },
     body: JSON.stringify({
         action: "scan",
         id: currentQR,
