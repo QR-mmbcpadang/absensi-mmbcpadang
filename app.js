772188641636185b9d689698ed7f83cf45b10849
QR Attendance <ipeyaja43@gmail.com>
2026-08-05 13:49:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const GAS_URL = "https://script.google.com/macros/s/AKfycbyokn5uN2NyiHanuwjafc158puU5B6SMR2HBWu2sCsM1e5k4VGwv_8oLuwJtg1_wpF9/exec";
+let lokasiAbsen = "";
 //==============================
 // JAM DIGITAL
 //==============================
