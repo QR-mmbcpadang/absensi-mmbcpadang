68d81273d9ccde48c2f4298d8ef802466a2a99df
QR Attendance <ipeyaja43@gmail.com>
2026-08-06 07:40:40 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,26 @@
-self.addEventListener("install", (e)=>{
+const CACHE_NAME = "MMBC-PADANG-v1.0.1";
+
+self.addEventListener("install", (event) => {
+
     console.log("Service Worker Installed");
+
+    self.skipWaiting();
+
 });
 
-self.addEventListener("fetch",(e)=>{});
+self.addEventListener("activate", (event) => {
+
+    console.log("Service Worker Activated");
+
+    event.waitUntil(
+        self.clients.claim()
+    );
+
+});
+
+self.addEventListener("fetch", (event) => {
+
+    // Untuk saat ini biarkan request langsung ke server
+    // Tidak melakukan cache agar selalu memakai file terbaru.
+
+});
